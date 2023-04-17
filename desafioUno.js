73f65ed14fe87b1be10a975818ef188e55177d8f
Camila Almada <camialmada23@gmail.com>
2023-04-17 20:54:26 -0300

    ultimas modificaciones

--- desafioUno.js
+++ desafioUno.js
@@ -3,7 +3,9 @@
 //metodo addProduct el que agregara un prod al arreglo de prod inicial.
 //validar q no se repita el campo "code", y q todos los campos sean obligatorios
 //al agregarlos debe creaerse un id autoincrementable
+
 // metodo getProducts debe devolver el arreglo con todos los prod creados hasta ese momento
+
 //metodo getProductById  debe buscar en el arreglo el prod q coincida con el id
 //en caso de no coincidir ningun id mostrar en consola un error not found 
 
@@ -16,20 +18,34 @@ class ProductManager {
 
     constructor(){
         this.products=[]; 
-        this.title= title;
-        this.description= description;
-        this.price=price;
-        this.thumbnail=thumbnail;
-        this.code=code;
-        this.stock= stock;
+        
 
     }
 
     
-    addProduct(){
+    addProduct(title, description, price, thumbnail, code, stock){
 
-        const producto = this.#getId();
-        this.products.push(producto)
+        if (!title || !description|| !price || !thumbnail || !code|| !stock){
+            console.log("Todos los campos son obligatorios");
+        }
+        let filter=this.products.filter((p)=>p.code===code)
+
+        if(filter.length>0){
+          console.log("Ese código ya existe"); 
+          return     
+        }
+
+        const producto ={
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        }
+
+         producto.id = this.#getId();
+         this.products.push(producto)
     }
 
     #getId(){
@@ -37,6 +53,38 @@ class ProductManager {
         this.#id += 1;
         return Idacum
     }
+
+     getProducts(){
+         return this.products
+     }
+
+     getProductById(idProductos){
+
+        const productoIndex= this.products.findIndex((prod)=>prod.id===idProductos)
+
+        if(productoIndex ===-1){
+            ("not found ")
+            return
+        }
+
+        const prod= this.products[productoIndex]
+        // else{
+        //     console.log(`El producto buscado es : ${this.products[productoIndex]} `);
+           
+        // }
+        
+     }
+
 }
 
+const productos = new ProductManager();
+
+productos.addProduct("celular","Alta gama",80000, ".../images/images.jpg", 158778,10)
+productos.addProduct("tablet","marca sony",50000, ".../images/images.jpg", 158778,10)
+productos.addProduct("tablet","",50000, ".../images/images.jpg", 158779,10)
+productos.getProductById(3)
+
+console.log(productos.getProducts());
+console.log(productos.getProductById());
     
+
